1186021024b3334f9a1dba61989b750e748d6299
Sara Baqla <missatrox44@gmail.com>
2022-12-01 09:11:55 -0600

    output Meal categories, no styling

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,17 @@
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+import { CATEGORIES } from '../data/dummy-data';
+
+//optional helper function to keep JSX code leaner in CategoriesScreen function
+function renderCategoryItem(itemData) {
+  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />;
+}
+
+function CategoriesScreen() {
+  return <FlatList
+    data={CATEGORIES}
+    keyExtractor={(item) => item.id}
+    renderItem={renderCategoryItem} />
+}
+
+export default CategoriesScreen;
