ab21dfefe62f18d592347c89547ea7d76bb72a9f
Sara Baqla <missatrox44@gmail.com>
2022-12-02 12:45:23 -0600

    used redux state in favorites screen component

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,20 +1,27 @@
 import { useContext } from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import Mealslist from '../components/MealsList/MealsList';
-import { FavoritesContext } from '../store/context/favorites-context';
+// import { FavoritesContext } from '../store/context/favorites-context';
 import { MEALS } from '../data/dummy-data';
+
+import { useSelector } from 'react-redux';
+
 
 function FavoritesScreen() {
   //need to get favorite meals from ids array
- const favoriteMealsCtx = useContext(FavoritesContext);
+  //CONTEXT API
+  //  const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids)
 
- //get all meals from raw data, check if id matches favorite mealsId array, if true then return new array
- const favoriteMeals = MEALS.filter(meal => 
-  favoriteMealsCtx.ids.includes(meal.id)
+  //get all meals from raw data, check if id matches favorite mealsId array, if true then return new array
+  const favoriteMeals = MEALS.filter(meal =>
+    //CONTEXT API
+    // favoriteMealsCtx.ids.includes(meal.id)
+    favoriteMealIds.includes(meal.id)
   );
 
   //create logic to render no favorites message
-  if (favoriteMeals.length === 0){
+  if (favoriteMeals.length === 0) {
     return (
       <View style={styles.rootContainer}>
         <Text style={styles.text}>
