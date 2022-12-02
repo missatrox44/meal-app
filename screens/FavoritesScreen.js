be08dd473c04430cefe393b22c3c67339ba1098f
Sara Baqla <missatrox44@gmail.com>
2022-12-02 11:16:04 -0600

    managed favorite meals with Context API

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,9 +1,48 @@
-import { Text } from 'react-native';
+import { useContext } from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+import Mealslist from '../components/MealsList/MealsList';
+import { FavoritesContext } from '../store/context/favorites-context';
+import { MEALS } from '../data/dummy-data';
 
 function FavoritesScreen() {
+  //need to get favorite meals from ids array
+ const favoriteMealsCtx = useContext(FavoritesContext);
+
+ //get all meals from raw data, check if id matches favorite mealsId array, if true then return new array
+ const favoriteMeals = MEALS.filter(meal => 
+  favoriteMealsCtx.ids.includes(meal.id)
+  );
+
+  //create logic to render no favorites message
+  if (favoriteMeals.length === 0){
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>
+          Womp womp womp.....
+        </Text>
+        <Text style={styles.text}>
+          You have no favorite meals yet.
+        </Text>
+      </View>
+    )
+  }
+
   return (
-    <Text>My favorite recipes.</Text>
+    <Mealslist items={favoriteMeals} />
   )
 }
 
 export default FavoritesScreen
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+})
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,7 +1,5 @@
 import { useLayoutEffect } from 'react';
-import { View, FlatList, StyleSheet, Text } from 'react-native';
-// import { FlatList } from 'react-native-web';
-import MealItem from '../components/MealItem';
+import Mealslist from '../components/MealsList/MealsList';
 import { MEALS, CATEGORIES } from '../data/dummy-data';
 
 function MealsOverviewScreen({ route, navigation }) {
@@ -23,39 +21,13 @@ function MealsOverviewScreen({ route, navigation }) {
       title: categoryTitle,
     });
   }, [catId, navigation]);
+ 
+  return <Mealslist items={displayedMeals}/>
 
-  function renderMealItem(itemData) {
-    const mealItemProps = {
-      id: itemData.item.id,
-      title: itemData.item.title,
-      imageUrl: itemData.item.imageUrl,
-      affordability: itemData.item.affordability,
-      complexity: itemData.item.complexity,
-      duration: itemData.item.duration
-    }
-
-    return (
-      <MealItem {...mealItemProps} />
-    )
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item => item.id)}
-        renderItem={renderMealItem} />
-    </View>
-  )
 }
 
 export default MealsOverviewScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16
-  }
-})
+
 
 //alternatively utilized useRoute if need access to currently load route info in some nested component not registered as a screen
